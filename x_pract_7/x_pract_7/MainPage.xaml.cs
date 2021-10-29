5c02e085b024da68f75c88e065c2c94403ca4874
Kote_Animen <i.filyak@kvantron.com>
2021-10-29 21:55:52 +0300

    Добавьте файлы проекта.

---
+++ x_pract_7/x_pract_7/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace x_pract_7
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isRun = false;
+        string message = "";
+        DateTime setDate;
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1), TimeTick);
+        }
+
+        private void btn_Set_Clicked(object sender, EventArgs e)
+        {
+            setDate = datePicker.Date + timePicker.Time;
+            message = ent_Message.Text;
+            datePicker.IsEnabled = false;
+            timePicker.IsEnabled = false;
+            ent_Message.IsEnabled = false;
+            isRun = true;
+        }
+
+        private void btn_Reset_Clicked(object sender, EventArgs e)
+        {
+            setDate = DateTime.MinValue;
+            datePicker.IsEnabled = true;
+            timePicker.IsEnabled = true;
+            ent_Message.IsEnabled = true;
+            isRun = false;
+        }
+        bool TimeTick()
+        {
+            if (isRun)
+            {
+                if(setDate <= DateTime.Now)
+                {                    
+                    DisplayAlert("Напиминание", message, "Ok");
+                    isRun = false;
+                    datePicker.IsEnabled = true;
+                    timePicker.IsEnabled = true;
+                    ent_Message.IsEnabled = true;
+                }
+            }
+            return true;
+        }
+    }
+}
